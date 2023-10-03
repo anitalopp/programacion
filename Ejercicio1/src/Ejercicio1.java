b6e10f0f99a10ec048c9fbd081862dd7f12fb00d
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-03 12:49:03 +0200

    value

---
+++ Ejercicio1/src/Ejercicio1.java
@@ -0,0 +1,9 @@
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        String name = "Ana";
+        System.out.println("Hola Mundo"); 
+        System.out.println("Mi nombre es " + name);
+    }
+}
---
+++ ejercicio 1/src/ejercicio2.java
@@ -0,0 +1,10 @@
+
+public class ejercicio1 {
+
+	public static void main (String[] args) {
+		String nombre="Ana";
+		System.out.println("Hola Mundo"); 
+		System.out.println("Mi nombre es Ana");
+		
+		}
+}
