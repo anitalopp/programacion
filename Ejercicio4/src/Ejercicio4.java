b4452365d431fdf9c5d1c3388e125a297a9d72b1
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-04 10:37:00 +0200

    Ejercicio4

---
+++ Ejercicio4/src/Ejercicio4.java
@@ -0,0 +1,25 @@
+
+public class Ejercicio4 {
+	public static void main(String[] args) {
+ 
+		 float XX=450;
+		 float YY=33;
+		 float ZZ;
+		 
+	ZZ = XX + YY;
+	System.out.println("El resultado de sumar " + XX + " y " + YY + " es " + ZZ );
+	
+	ZZ = XX - YY;
+	System.out.println("El resultado de restar " +  XX + " y " + YY + " es " + ZZ );
+	
+	ZZ = XX * YY;
+	System.out.println("El resultado de multplicar " + XX + " y " + YY + " es " + ZZ );
+
+	ZZ = XX / YY;
+	System.out.println("El resultado de dividir " + XX + " y " + YY + " es " + ZZ );
+
+	ZZ = XX % YY;
+	System.out.println("El resto de la división " + XX + " y " + YY + " es " + ZZ );
+
+}
+}
