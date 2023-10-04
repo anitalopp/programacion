50790cdaf4a945a693cc8002a42642b67bc6a40f
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-04 10:34:44 +0200

    Ejercicio3

---
+++ Ejercicio3/src/Ejercicio3.java
@@ -0,0 +1,13 @@
+
+public class Ejercicio3 {
+	public static void main(String[] args) {
+ 
+		 float f= 150f;
+		 float c;
+		 
+		 c = (5 / (9 * (f - 32)));
+	
+		 System.out.println("El resultado de convertir " + f + " grados Fahrenheit a grados centígrados es: " + c);
+	}
+		
+}
