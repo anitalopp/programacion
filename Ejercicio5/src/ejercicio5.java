a5309761a320c12bba1244ac42bd26417158ac44
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-09 09:46:53 +0200

    Ejercicio5

---
+++ Ejercicio5/src/ejercicio5.java
@@ -0,0 +1,53 @@
+
+public class ejercicio5 {
+	public static void main(String[] args) {
+	
+			float num5= 5;
+			float num8= 8;
+			float num6= 6;
+			float num9= 9;
+			float num2= 2;
+			float num0= 0;
+			float num3= 3;
+			float num1= 1;
+			float num55= 55;
+			float num20= 20;
+			float num15= 15;
+			
+			float resultado;
+			
+resultado = (-num5 + (num8 * num6));
+System.out.println("El resultado de la operación -5+8*6 es: " + resultado);
+
+resultado = (num55 + num9)%(num9);
+System.out.println("El resultado de la operación (55+9)%9 es: " + resultado);
+
+resultado = num20 + ((-num3 * num5) / num8);
+System.out.println("El resultado de la operación 20+ -3*5/8 es: " + resultado);
+
+resultado = num5 + num15 / (num3 * num2) - (-num8 % num3);
+System.out.println("El resultado de la operación 5+15/3*2-8%3 es: " + resultado);
+
+
+
+			
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
