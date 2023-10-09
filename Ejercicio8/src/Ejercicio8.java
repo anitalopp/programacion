f3e4255710e9bc8c62697124e8a8df2178d16326
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-09 10:45:35 +0200

    Ejercicio8

---
+++ Ejercicio8/src/Ejercicio8.java
@@ -0,0 +1,15 @@
+public class Ejercicio8 {
+	public static void main(String[] args) {
+			
+		float pulgadas= 25f;
+		float conversion;
+		final float FACTOR_CONVERSION=39.37f;
+		conversion= (pulgadas / 39.37f);
+		
+		System.out.println("El resultado de la conversión es " + conversion);
+
+		
+		
+		
+	}
+}
