9b9eee766b161d4804eb99ec8de6b62dee525f67
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-19 14:04:13 +0200

    17/10

--- Ejercicio8/src/Ejercicio8.java
+++ Ejercicio8/src/Ejercicio8.java
@@ -2,6 +2,18 @@
 public class Ejercicio8 {
 	public static void main (String [] args) {
 		
+		int A = 5;
+		int B = 6;
+		int C = 9;
+		
+		if (A < B & A > C) {
+			System.out.println("El número A: " + A + " es el mayor");
+		} else if (B > A & B > C) {
+			System.out.println("El número B: " + B + " es el mayor");
+		} else {
+			System.out.println("El número C: " + C + " es el mayor");
+		}
+		
 		
 		
 		
