a7e57e7782ff650c93e8adcbf5905bf5b1905a6d
alumnofp <alumnofp@FPA09.CALASANZSALAMANCA.local>
2023-10-20 10:16:01 +0200

    20/10

--- Ejercicio09/src/Ejercicio09.java
+++ Ejercicio09/src/Ejercicio09.java
@@ -2,21 +2,21 @@ public class Ejercicio09 {
 	
 	public static void main(String[] args) {
 		
-		int variableX = 5;
-		int variableY = 1;
+		int x = 5;
+		int y = 1;
 		
-		 if (variableX > 0 && variableY > 0) {
-		     System.out.println("El punto x = " + variableX + ", y = " + variableY + " pertenece al cuadrante I.");
-		 } else if (variableX < 0 && variableY > 0) {
-		     System.out.println("El punto x = " + variableX + ", y = " + variableY + " pertenece al cuadrante II.");
-		 } else if (variableX < 0 && variableY < 0) {
-		     System.out.println("El punto x = " + variableX + ", y = " + variableY + " pertenece al cuadrante III.");
-		 } else if (variableX > 0 && variableY < 0) {
-		     System.out.println("El punto x = " + variableX + ", y = " + variableY + " pertenece al cuadrante IV.");
-		 } else if (variableX == 0 && variableY == 0) {
-		     System.out.println("El punto x = " + variableX + ", y = " + variableY + " pertenece al origen.");
+		 if (x > 0 && y > 0) {
+		     System.out.println("El punto x = " + x + ", y = " + y + " pertenece al cuadrante I.");
+		 } else if (x < 0 && y > 0) {
+		     System.out.println("El punto x = " + x + ", y = " + y + " pertenece al cuadrante II.");
+		 } else if (x < 0 && y < 0) {
+		     System.out.println("El punto x = " + x + ", y = " + y + " pertenece al cuadrante III.");
+		 } else if (x > 0 && y < 0) {
+		     System.out.println("El punto x = " + x + ", y = " + y + " pertenece al cuadrante IV.");
+		 } else if (x == 0 && y == 0) {
+		     System.out.println("El punto x = " + x + ", y = " + y + " pertenece al origen.");
 		 } else {
-		 System.out.println("El punto x = " + variableX + ", y = " + variableY + " no pertenece a un cuadrante ni al origen.");
+		 System.out.println("El punto x = " + x + ", y = " + y + " no pertenece a un cuadrante ni al origen.");
 		 }     		
 	}
 
---
+++ Ejercicio15/src/Ejercicio15.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio15 {
+	public static void main (String [] args) {
+		
+		int limite = 10;
+		int semilla1 = 0;
+		int semilla2 = 1;
+		
+		System.out.println("Serie de fibonacci con límite " + limite + " numeros y con semillas " + semilla1 + " y " + semilla2 + " es: " );
+		
+		for (int i = 0; i < limite; i++)
+			System.out.print(semilla1 + " "); {
+	
+	}
+}
