96de40abb8820c9497db66ab8c26da2195317eec
anitalopp <analopezjuan05@gmail.com>
2023-10-24 22:58:45 +0200

    24/10

--- Ejercicio16/src/Ejercicio16.java
+++ Ejercicio16/src/Ejercicio16.java
@@ -1,9 +1,39 @@
 
 public class Ejercicio16 {
-	public static void main (String [] args) {
-		
-		
-		
-		
-	}
+
+		public static void main (String [] args) {
+			int dia = 3;
+				
+			switch (dia) {
+			
+			case 1:
+					System.out.println("Es lunes ");
+			break;
+			case 2:
+				System.out.println("Es martes ");
+			break;
+			case 3:
+				System.out.println("Es miércoles ");
+			break;
+			case 4: 
+				System.out.println("Es jueves ");
+			break;
+			case 5:
+				System.out.println("Es viernes ");
+			break;
+			case 6:
+				System.out.println("Es sábado ");
+			break;
+			case 7:
+				System.out.println("Es domingo ");
+			break;
+			default:
+				System.out.println("No es ningún día te amo kike");
+			
+			
+			
+			}
+			
+			
+		}
 }
---
+++ Ejercicio27/src/Ejercicio27.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio27 {
+	public static void main (String []  args) {
+		
+	int albuminica = 2;
+	float hemoglobina = 10f;
+	boolean masculino = true;
+	int estadio = 4;
+	int edad = 25;
+	int recuento = 12000;
+	int linfocitos_mm3 = 550;
+	int linfocitos_p = 5;
+		
+	
+	
+	
+	
+	
+	}
+}
